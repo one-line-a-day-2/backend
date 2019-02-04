2d2f76a97a3df3940f367ce32e369178c3dfdb08
Thomas Claydon <tomclaydon102@gmail.com>
2019-02-04 17:07:42 -0500

    heroku7

--- api/database/dbConfig.js
+++ api/database/dbConfig.js
@@ -1,4 +1,5 @@
 const knex = require('knex');
 const knexConfig = require('../../knexfile.js');
-const client = knexConfig.development;
+
+const client = knexConfig.development.connect();
 module.exports = knex(client);
