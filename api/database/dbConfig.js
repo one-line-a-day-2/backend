10cf9c6fbd7760b7f53e01ba37e2d40605476ca6
Thomas Claydon <tomclaydon102@gmail.com>
2019-02-04 17:30:08 -0500

    her

--- api/database/dbConfig.js
+++ api/database/dbConfig.js
@@ -1,5 +1,4 @@
 const knex = require('knex');
 const knexConfig = require('../../knexfile.js');
-
 const client = knexConfig.development;
 module.exports = knex(client);
--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,13 @@
 // Update with your config settings.s
-require('dotenv').config();
+const localPg = {
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
+};
+
+const dbConnection = process.env.DATABASE_URL || localPg;
+
 const pg = require('pg');
 pg.defaults.ssl = true;
 
@@ -8,7 +16,7 @@ module.exports = {
     // client: 'sqlite3',
     client: 'pg',
     // connection: { filename: './api/database/auth.db3' }, // change this if you want a different name for the database
-    connection: process.env.DATABASE_URL,
+    connection: dbConnection,
     useNullAsDefault: true, // used to avoid warning on console
     migrations: {
       directory: './api/database/migrations',
