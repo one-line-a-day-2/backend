6d55efd6aadf687afa40b78873b7a21fbb289d34
Thomas Claydon <tomclaydon102@gmail.com>
2019-02-05 14:14:54 -0500

    fixxin

--- api/database/dbConfig.js
+++ api/database/dbConfig.js
@@ -1,5 +1,5 @@
 ///
 const knex = require('knex');
 const knexConfig = require('../../knexfile.js');
-const client = knexConfig["development"];
+const client = knexConfig["production"];
 module.exports = knex(client);
