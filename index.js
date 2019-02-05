d029d05c7279cd92124b93ec74397e4132260336
Thomas Claydon <tomclaydon102@gmail.com>
2019-02-04 19:29:04 -0500

    heroku11

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const { server } = require('./api/server.js');
 console.log(process.env.DB_ENV);
 if(process.env.DB_ENV === 'development'){
   port = 5000;
-}else{
+}else if(process.env.DB_ENV === 'production'){
   port = process.env.PORT;
 }
-
+console.log(port);
 server.listen(port, () => {
   console.log(`Server listening ${port}`);
 });
